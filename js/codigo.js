b89d806e42b46976864f3a1aea562d24929317e2
Diana Di Stefano <diana.distefano@gmail.com>
2020-08-27 20:48:16 +0200

    Funcion para ver días pares de la semana

---
+++ js/codigo.js
@@ -0,0 +1,14 @@
+var dias = ['Lunes','Martes','Miércoles','Jueves','Viernes','Sábado','Domingo'];
+
+function diasSemana() {
+    console.log('Los días pares de la semana son: ');
+    for (let i=0; i<dias.length; i++) {
+        if (i===(dias.length-1)) {
+            console.log(`El ${dias[i]} es el último y séptimo día de la semana`)
+        }
+        
+        else if(i%2 != 0) {
+            console.log(`${dias[i]} (${[i+1]})`)
+        }
+    }
+}
